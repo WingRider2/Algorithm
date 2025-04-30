9f6c72a3320ff2f16b78193cd0621d0673f0975b
WingRider2 <72336626+WingRider2@users.noreply.github.com>
2025-04-30 17:59:48 +0900

    [Bronze V] Title: 새로운 시작, Time: 44 ms, Memory: 5328 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cs
@@ -0,0 +1,19 @@
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private const int bufferSize = 131072;
+        public static readonly StreamReader sr = new(new BufferedStream(Console.OpenStandardInput(), bufferSize));
+        public static readonly StreamWriter sw = new(new BufferedStream(Console.OpenStandardOutput(), bufferSize));
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("파이팅!!");
+        }
+
+    }
+
+}
