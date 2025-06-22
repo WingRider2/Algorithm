8326e24fd1f28a9ba1bb31d20bb31a3b5cb8180d
WingRider2 <72336626+WingRider2@users.noreply.github.com>
2025-06-22 20:35:05 +0900

    [level 0] Title: k의 개수, Time: 15.36 ms, Memory: 36 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.cs
@@ -0,0 +1,17 @@
+    using System;
+    using System.IO;
+    using System.Text;
+    using System.Linq;
+
+
+public class Solution {
+    public int solution(int i, int j, int k) {
+
+        StringBuilder sb = new StringBuilder();
+        for (int q = i; q <= j; q++)
+        {
+            sb.Append(q.ToString());
+        }            
+        return sb.ToString().Count(c => c == k+'0');
+    }
+}
