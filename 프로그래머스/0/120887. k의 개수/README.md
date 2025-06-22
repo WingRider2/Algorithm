6a008954b4b5d43fc78c1e96ae6737a97d4ed54d
WingRider2 <72336626+WingRider2@users.noreply.github.com>
2025-06-22 20:49:31 +0900

    [level 0] Title: k의 개수, Time: 14.46 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/README.md
+++ 프로그래머스/0/120887. k의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 0] k의 개수 - 120887 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120887?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120887) 
 
 ### 성능 요약
 
-메모리: 36 MB, 시간: 15.36 ms
+메모리: 35.9 MB, 시간: 14.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 22일 20:35:02
+2025년 06월 22일 20:49:29
 
 ### 문제 설명
 
--- 프로그래머스/0/120887. k의 개수/k의 개수.cs
+++ 프로그래머스/0/120887. k의 개수/k의 개수.cs
@@ -1,17 +1,12 @@
-    using System;
-    using System.IO;
-    using System.Text;
-    using System.Linq;
+using System;
+using System.Text;
+using System.Linq;
 
 
 public class Solution {
     public int solution(int i, int j, int k) {
-
         StringBuilder sb = new StringBuilder();
-        for (int q = i; q <= j; q++)
-        {
-            sb.Append(q.ToString());
-        }            
+        for (int q = i; q <= j; q++) sb.Append(q.ToString());          
         return sb.ToString().Count(c => c == k+'0');
     }
 }
