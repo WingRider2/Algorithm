681ce96bc2f3654f66e87c8b39c9c1761f134200
WingRider2 <72336626+WingRider2@users.noreply.github.com>
2025-08-29 16:25:56 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 52 ms, Memory: 5516 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cs
@@ -0,0 +1,35 @@
+namespace ConsoleApp1
+{
+    using System.Numerics;
+    using System.Text;
+    using System;         
+    using System.IO;
+
+    internal class Program
+    {
+        private const int bufferSize = 131072;
+        public static readonly StreamReader sr = new(new BufferedStream(Console.OpenStandardInput(), bufferSize));
+        public static readonly StreamWriter sw = new(new BufferedStream(Console.OpenStandardOutput(), bufferSize));
+        public static StringBuilder sb = new StringBuilder();
+        static void Main(string[] args)
+        {
+            int n = int.Parse(sr.ReadLine());
+
+            if (n <= 1) { sw.WriteLine(1); sw.Flush(); return; }
+            
+            int a = 1;  
+            int b = 3; 
+            for (int i = 3; i <= n; i++)
+            {
+                int c = (a + a + b) % 10007;
+                a = b;
+                b = c;
+            }
+            //sb.AppendLine(art);            
+            sw.WriteLine(b);
+            sw.Flush();
+        }
+
+    }
+
+}
