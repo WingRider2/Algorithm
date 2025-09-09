89ba0e5d5dfb9a262f23536c806dca8ca58d2648
WingRider2 <72336626+WingRider2@users.noreply.github.com>
2025-09-09 16:33:38 +0900

    [Bronze V] Title: CASIO, Time: 48 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cs
@@ -0,0 +1,11 @@
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            
+            Console.WriteLine(s[0]==s[1]? 1 : 0);
+            //Console.WriteLine(sb.ToString());
+        }
+
+    }
