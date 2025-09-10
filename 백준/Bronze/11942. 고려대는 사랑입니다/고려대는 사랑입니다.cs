6ec7b40ea40ee4b57e6ab86d875015366c84e5b8
WingRider2 <72336626+WingRider2@users.noreply.github.com>
2025-09-10 18:46:18 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 44 ms, Memory: 5344 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.cs
@@ -0,0 +1,18 @@
+namespace ConsoleApp1
+{
+
+    internal class Program
+    {
+        private const int bufferSize = 131072;//131072=128KB. 경험적 가장빠른속도. 기본=4096=4KB.
+        public static readonly StreamReader sr = new(new BufferedStream(Console.OpenStandardInput(), bufferSize));
+        public static readonly StreamWriter sw = new(new BufferedStream(Console.OpenStandardOutput(), bufferSize));
+
+        static void Main(string[] args)
+        {
+              sw.WriteLine($"고려대학교");        
+            sw.Flush();
+        }   
+       
+    }
+    
+}
